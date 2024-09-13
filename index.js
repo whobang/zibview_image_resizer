96269d943d4c08fb6cb30f55ef59bd97f298f80e
hyunha <hyunha.no@sustinvest.com>
2024-09-13 09:10:09 +0900

    이미지 리사이즈 개발 중

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 const querystring = require("querystring");
 const express = require("express");
 const sharp = require("sharp");
-const fs = require("fs");
+const fs = require("fs-extra");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 const PORT = 4000;
-const SMALL_SIZE_OUTPUT_PATH = "";
-const LARGE_SIZE_OUTPUT_PATH = "";
+const OUTPUT_PATH = "assets/images/resize";
 
 // Image types that can be handled by Sharp
 const SUPPORTED_IMAGE_TYPES = [
@@ -25,16 +25,19 @@ const SUPPORTED_IMAGE_TYPES = [
 
 // 이미지 변환 라우터
 app.get("/api/image-resize", async (req, res) => {
-  const { filePath, size, type, quality, format } = querystring.parse(
-    req.querystring
-  );
+  console.log(req.query);
+  const { filePath, size } = req.query;
 
   if (!filePath) {
     return res.status(400).send("파일 경로가 필요합니다.");
   }
 
+  if (!size || (size != "s" && size != "l")) {
+    return res.status(400).send("파일 사이즈가 잘못 입력되었습니다.");
+  }
+
   // 파일 확장자 및 기본 이름 추출
-  const fileExtension = path.extname(filePath).toLowercase();
+  const fileExtension = path.extname(filePath).substring(1).toLowerCase();
 
   // 지원되지 않는 파일 형식 확인
   if (!SUPPORTED_IMAGE_TYPES.includes(fileExtension)) {
@@ -42,12 +45,15 @@ app.get("/api/image-resize", async (req, res) => {
   }
 
   // 파일 경로 유효성 검사
-  if (fs.existsSync(filePath)) {
+  if (fs.ensureFileSync(filePath)) {
     return res.status(404).send("파일을 찾을 수 없습니다.");
   }
 
   // 1. 이미 이미지가 변환되었는지 확인
   // 2. 변환된 이미지가 없다면 이미지 변환
+  const outputPath = path.join(OUTPUT_PATH, size, uuidv4());
+  console.log("outputPath:", outputPath);
+  await sharp(filePath).toFormat("webp").toFile(outputPath);
 });
 
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
